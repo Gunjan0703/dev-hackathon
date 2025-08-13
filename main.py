2f220a34dfa45aa13533b6a68f66fa7bd268afc4
Gunjan <164139238+Gunjan0703@users.noreply.github.com>
2025-08-13 19:08:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,5 +12,6 @@ def hello():
 
 
 if __name__ == '__main__':
+    # Get port from environment variable or default to 80
     port = int(os.environ.get('PORT', 80))
     app.run(host='0.0.0.0', port=port)
